83f2771f6f1e842e5bad0ce376fdb8d2d5bb438f
mipacamere <studiosmipa@gmail.com>
2024-11-23 11:04:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toggleContent(id) {
     const content = document.getElementById(id);
+    const button = content.previousElementSibling;  // Trova il tasto relativo alla sezione
+
+    // Controlliamo se il contenuto è nascosto o visibile
     if (content.style.display === "none" || content.style.display === "") {
         content.style.display = "block";
+        button.textContent = "-";  // Cambia il tasto a "-"
     } else {
         content.style.display = "none";
+        button.textContent = "+";  // Cambia il tasto a "+"
     }
 }
