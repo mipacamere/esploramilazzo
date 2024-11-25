540b757e6417cb57aaf302085c30962ab1c40b10
mipacamere <studiosmipa@gmail.com>
2024-11-25 12:57:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function nextStep(step) {
-    // Nascondi tutte le sezioni
-    const allSteps = document.querySelectorAll('.container > div');
-    allSteps.forEach(step => {
-        step.classList.add('hidden');
-    });
 
-    // Mostra la sezione successiva
-    const nextStep = document.getElementById(step);
-    if (nextStep) {
-        nextStep.classList.remove('hidden');
-    }
-}
-
-// Mostra la prima sezione al caricamento
-document.addEventListener('DOMContentLoaded', function() {
-    nextStep('step1');
-});
