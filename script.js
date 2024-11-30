ee91ca304f590ba7a1e00289d682170b1fccbc5e
mipacamere <studiosmipa@gmail.com>
2024-11-30 14:42:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function hideAllSections() {
-    const sections = document.querySelectorAll('.container > div');
-    sections.forEach(section => section.classList.add('hidden'));
-}
-
+function hideAllSections() { const sections = document.querySelectorAll('.container > div'); sections.forEach(section => section.classList.add('hidden')); }
 // Funzioni per navigazione tra le sezioni
 function goToArrivo() { hideAllSections(); document.getElementById('arrivo').classList.remove('hidden'); }
 function goToIndicazioni() { hideAllSections(); document.getElementById('indicazioni').classList.remove('hidden'); }
@@ -17,99 +13,28 @@ function goToPranzoA() { hideAllSections(); document.getElementById('pranzoA').c
 function goToCastelloA() { hideAllSections(); document.getElementById('castelloA').classList.remove('hidden'); }
 function goToPomeriggioA() { hideAllSections(); document.getElementById('pomeriggioA').classList.remove('hidden'); }
 function goToBaiaDelTonoA() { hideAllSections(); document.getElementById('baiaDelTonoA').classList.remove('hidden'); }
-function goTempoSpiaggiaA() {
-    hideAllSections();
-    document.getElementById('tempoSpiaggiaA').classList.remove('hidden');
-}
-function goToAttivitaSpiaggiaA() {
-    hideAllSections();
-    document.getElementById('attivitaSpiaggiaA').classList.remove('hidden');
-}
-function goToTramontoTonoA() {
-    hideAllSections();
-    document.getElementById('tramontoTonoA').classList.remove('hidden');
-}
-function goToRientroCentroA() {
-    hideAllSections();
-    document.getElementById('rientroCentroA').classList.remove('hidden');
-}
-function goToSpiaggiaPonente() {
-    hideAllSections();
-    document.getElementById('spiaggiaPonente').classList.remove('hidden');
-}
-
-function goToLidoSpiaggia() {
-    hideAllSections();
-    document.getElementById('lidoSpiaggia').classList.remove('hidden');
-}
-function goToAttivitaAcquaticheB() {
-    hideAllSections();
-    document.getElementById('attivitaAcquaticheB').classList.remove('hidden');
-}
-function goToPranzoB() {
-    hideAllSections();
-    document.getElementById('pranzoB').classList.remove('hidden');
-}
-function goToPomeriggioB() {
-    hideAllSections();
-    document.getElementById('pomeriggioB').classList.remove('hidden');
-}
-function goToBorgoAnticoB() {
-    hideAllSections();
-    document.getElementById('borgoAnticoB').classList.remove('hidden');
-}
-function goToCastelloB() {
-    hideAllSections();
-    document.getElementById('castelloB').classList.remove('hidden');
-}
-function goToCapoMilazzoB() {
-    hideAllSections();
-    document.getElementById('capoMilazzoB').classList.remove('hidden');
-}
-function goToIndicazionicapomilazzoB() {
-    hideAllSections();
-    document.getElementById('indicazionicapomilazzoB').classList.remove('hidden');
-}
-function goToPiscinaVenereB() {
-    hideAllSections();
-    document.getElementById('piscinaVenereB').classList.remove('hidden');
-}
-function goToPanoramicaIsoleB() {
-    hideAllSections();
-    document.getElementById('panoramicaIsoleB').classList.remove('hidden');
-}
-function goToTramontoCapoMilazzo() {
-    hideAllSections();
-    document.getElementById('tramontoCapoMilazzo').classList.remove('hidden');
-}
-function goToRientroCentroB() {
-    hideAllSections();
-    document.getElementById('rientroCentroB').classList.remove('hidden');
-}
-function goToCena() {
-    hideAllSections();
-    document.getElementById('cena').classList.remove('hidden');
-}
-function goToPasseggiata() {
-    hideAllSections();
-    document.getElementById('passeggiata').classList.remove('hidden');
-}
-function goToGelato() {
-    hideAllSections();
-    document.getElementById('gelato').classList.remove('hidden');
-}
-function goToBar() {
-    hideAllSections();
-    document.getElementById('bar').classList.remove('hidden');
-}
-function fineGiornata() {
-    hideAllSections();
-    document.getElementById('fineGiornata').classList.remove('hidden');
-}
-function goToIntro() { hideAllSections(); 
-document.getElementById('intro').classList.remove('hidden'); }
+function goTempoSpiaggiaA() { hideAllSections(); document.getElementById('tempoSpiaggiaA').classList.remove('hidden'); }
+function goToAttivitaSpiaggiaA() { hideAllSections(); document.getElementById('attivitaSpiaggiaA').classList.remove('hidden'); }
+function goToTramontoTonoA() { hideAllSections(); document.getElementById('tramontoTonoA').classList.remove('hidden'); }
+function goToRientroCentroA() { hideAllSections(); document.getElementById('rientroCentroA').classList.remove('hidden'); }
+function goToSpiaggiaPonente() { hideAllSections(); document.getElementById('spiaggiaPonente').classList.remove('hidden'); }
+function goToLidoSpiaggia() { hideAllSections(); document.getElementById('lidoSpiaggia').classList.remove('hidden'); }
+function goToAttivitaAcquaticheB() { hideAllSections(); document.getElementById('attivitaAcquaticheB').classList.remove('hidden'); }
+function goToPranzoB() { hideAllSections(); document.getElementById('pranzoB').classList.remove('hidden'); }
+function goToPomeriggioB() { hideAllSections(); document.getElementById('pomeriggioB').classList.remove('hidden'); }
+function goToBorgoAnticoB() { hideAllSections(); document.getElementById('borgoAnticoB').classList.remove('hidden'); }
+function goToCastelloB() { hideAllSections(); document.getElementById('castelloB').classList.remove('hidden'); }
+function goToCapoMilazzoB() { hideAllSections(); document.getElementById('capoMilazzoB').classList.remove('hidden'); }
+function goToIndicazionicapomilazzoB() { hideAllSections(); document.getElementById('indicazionicapomilazzoB').classList.remove('hidden'); }
+function goToPiscinaVenereB() { hideAllSections(); document.getElementById('piscinaVenereB').classList.remove('hidden'); }
+function goToPanoramicaIsoleB() { hideAllSections(); document.getElementById('panoramicaIsoleB').classList.remove('hidden'); }
+function goToTramontoCapoMilazzo() { hideAllSections(); document.getElementById('tramontoCapoMilazzo').classList.remove('hidden'); }
+function goToRientroCentroB() { hideAllSections(); document.getElementById('rientroCentroB').classList.remove('hidden'); }
+function goToCena() { hideAllSections(); document.getElementById('cena').classList.remove('hidden'); }
+function goToPasseggiata() { hideAllSections(); document.getElementById('passeggiata').classList.remove('hidden'); }
+function goToGelato() { hideAllSections(); document.getElementById('gelato').classList.remove('hidden'); }
+function goToBar() { hideAllSections(); document.getElementById('bar').classList.remove('hidden'); }
+function fineGiornata() { hideAllSections(); document.getElementById('fineGiornata').classList.remove('hidden'); }
+function goToIntro() { hideAllSections(); document.getElementById('intro').classList.remove('hidden'); }
 // Funzione per nascondere tutte le sezioni
-function hideAllSections() {
-    const sections = document.querySelectorAll('.container > div');
-    sections.forEach(section => section.classList.add('hidden'));
-}
+function hideAllSections() { const sections = document.querySelectorAll('.container > div'); sections.forEach(section => section.classList.add('hidden')); }
