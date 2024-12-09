42db3c7cb703d56d5edb67b27a1209d0f6fa446b
mipacamere <studiosmipa@gmail.com>
2024-12-09 12:10:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,6 @@ function toggleTheme() {
     const body = document.body;
     body.classList.toggle('dark-theme');
     body.classList.toggle('light-theme');
-     }
 
     const button = document.querySelector('.theme-toggle');
     if (body.classList.contains('dark-theme')) {
