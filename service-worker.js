1b03a682b566326f40ccd9a29b0864e2d440579a
mipacamere <studiosmipa@gmail.com>
2025-02-17 14:19:24 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const cacheName = 'my-pwa-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Quando il service worker è installato
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Quando il service worker è attivo e gestisce richieste
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
